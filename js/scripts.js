6fe917f2cfbeb24d4a793f3004f2f09d385e0e19
Golga <golgarud@live.fr>
2018-02-07 20:24:29 +0100

    CORE Offline rendering (Without UTF-8 support)

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,32 @@
 $(function(){
+	if ( window.location.href.indexOf("file://") === -1 )
+	{
+		$.get( window.location.href, render );
+	}
+	else
+	{
+		render(  $("body pre").html() );
+		// data = furiganaIt( markdown.toHTML( $("body pre").html() , "Maruku" ) );
+		// $("body").html( '<main id="main">'
+		// 	+ '<header>'
+		// 	+ '	<h1>'
+		// 	+ '		日本語 USJ1'
+		// 	+ '	</h1>'
+		// 	+ '</header>'
+		// 	+ '<div class="container">'
+		// 	+ '<div class="chapter">'
+		// 	+ data
+		// 	+ '	</div>'
+		// 	+ '	</div>' );
+	}
+});
+
+render = data => {
 	$("head").html('<meta charset="UTF-8">'
 		+ '<meta name="viewport" content="width=device-width, initial-scale=1">'
 		+ '<link rel="profile" href="http://gmpg.org/xfn/11">'
 		+ '<title> MarkdownQuantum Document </title>');
-	$.get( window.location.href , function( data ) {
-		data = furiganaIt( markdown.toHTML( data, "Maruku" ) );
+	data = furiganaIt( markdown.toHTML( data, "Maruku" ) );
 		$("body").html( '<main id="main">'
 		+ '<header>'
 		+ '	<h1>'
@@ -16,6 +38,4 @@ $(function(){
 		+ data
 		+ '	</div>'
 		+ '	</div>' );
-	});
-
-});
+}
