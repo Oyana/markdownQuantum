a3c818828001941ac2c7956f8bb7af74151328ce
Golga <golgarud@live.fr>
2018-02-07 21:08:06 +0100

    FIX undefined title constant

--- js/scripts.js
+++ js/scripts.js
@@ -1,41 +1,44 @@
 $(function(){
-	if ( window.location.href.indexOf("file://") === -1 )
+	const curentUrl = window.location.href;
+
+	if ( curentUrl.indexOf("file://") === -1 )
 	{
-		$.get( window.location.href, render );
+		/*Ajax hack to pache encoding without bom*/
+		$.get( curentUrl, render );
 	}
 	else
 	{
 		render(  $("body pre").html() );
-		// data = furiganaIt( markdown.toHTML( $("body pre").html() , "Maruku" ) );
-		// $("body").html( '<main id="main">'
-		// 	+ '<header>'
-		// 	+ '	<h1>'
-		// 	+ '		日本語 USJ1'
-		// 	+ '	</h1>'
-		// 	+ '</header>'
-		// 	+ '<div class="container">'
-		// 	+ '<div class="chapter">'
-		// 	+ data
-		// 	+ '	</div>'
-		// 	+ '	</div>' );
 	}
 });
 
 render = data => {
-	$("head").html('<meta charset="UTF-8">'
+	const $head = $("head");
+	const $body = $("body");
+	data = furiganaIt( markdown.toHTML( data, "Maruku" ) );
+
+	$head.html('<meta charset="UTF-8">'
 		+ '<meta name="viewport" content="width=device-width, initial-scale=1">'
 		+ '<link rel="profile" href="http://gmpg.org/xfn/11">'
 		+ '<title> MarkdownQuantum Document </title>');
-	data = furiganaIt( markdown.toHTML( data, "Maruku" ) );
-		$("body").html( '<main id="main">'
+		$body.html( '<main id="main">'
 		+ '<header>'
-		+ '	<h1>'
-		+ '		日本語 USJ1'
+		+ '	<h1 class="title main-title">'
+		+ '		MarkdownQuantum Document...'
 		+ '	</h1>'
 		+ '</header>'
 		+ '<div class="container">'
 		+ '<div class="chapter">'
 		+ data
 		+ '	</div>'
-		+ '	</div>' );
+		+ '	</div>'
+	);
+
+	/*Set main title in fixed header*/
+	const title = $body.find("h1")[1];
+	if ( title != void 0 )
+	{
+		$body.find(".main-title").html( title.innerHTML );
+		title.remove();
+	}
 }
